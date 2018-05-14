8b7e6191bdc74a7b00d08cdf3b11e67bead273ef
yasserkabbout <yasserkabbout@gmail.com>
2018-05-14 16:17:30 +0300

    Implmentation of the configuration file. The twitter account authentication details should be added to a config.txt in the provided format.

--- tweeLights.py
+++ tweeLights.py
@@ -2,36 +2,55 @@
 import tweepy, codecs, os, sys
 from aylienapiclient import textapi
 
+
 def setupTwitterAuth(file):
+	global authentication_data
 	if not os.path.isfile("config.txt"):
 		print("File path {} does not exist. Exiting...".format("config.txt"))
 		sys.exit()
 	try:
-		file=codecs.open("Config.txt", "r", "utf-8")
+		file=codecs.open("config.txt", "r", "utf-8")
 		authentication_data= file.readlines();
+
 		if authentication_data[0].__contains__("consumer_key"):
 			authentication_data[0]= authentication_data[0].lstrip("consumer_key:")
 			authentication_data[0]=authentication_data[0].strip()
 			print(authentication_data[0])
 		else:
 			raise (ValueError)
+
 		if authentication_data[1].__contains__("consumer_secret:"):
 			authentication_data[1]=authentication_data[1].lstrip("consumer_secret:")
 			authentication_data[1]=authentication_data[1].strip()
 			print(authentication_data[1])
+		else:
+			raise (ValueError)
 
+		if authentication_data[2].__contains__("access_token:"):
+			authentication_data[2]=authentication_data[2].lstrip("access_token:")
+			authentication_data[2]=authentication_data[2].strip()
+			print(authentication_data[2])
+		else:
+			raise (ValueError)
 
+		if authentication_data[3].__contains__("access_token_secret:"):
+			authentication_data[3]=authentication_data[3].lstrip("access_token_secret:")
+			authentication_data[3]=authentication_data[3].strip()
+			print(authentication_data[3])
+		else:
+			raise(ValueError)
 
 	except ValueError:
 		print("The data is not formatted as required whithin your config.txt file. Please reformat it by referring to the user manual!")
 
 
+setupTwitterAuth("config.txt")
 
 ## fill in your Twitter credentials
-consumer_key = 'n4RXGtbkQs3WSfgcJb4YfJ9yc'
-consumer_secret = 'edUBqt7WcGessBTY6K7C9ftS1QOtriT9ZjOYTVEeyH8cHPsJD0'
-access_token = '3449013974-ZWxCu7yzolQrS45Aojtswebe7lHFN81L15rToGo'
-access_token_secret = 'OaLVyPlirIMKv5b14QvW0y8MIES96hD6wb38LoUia7RaJ'
+consumer_key = authentication_data[0]
+consumer_secret = authentication_data[1]
+access_token = authentication_data[2]
+access_token_secret = authentication_data[3]
 
 
 ## let Tweepy set up an instance of the REST API
@@ -41,7 +60,7 @@ api = tweepy.API(auth)
 
 ## fill in your search query and store your results in a variable
 results = api.search(q = "@tweeLightsSWE", lang = "en", result_type = "recent", count = 10000)
-setupTwitterAuth("config.txt")
+
 
 ## use the codecs library to write the text of the Tweets to a .txt file
 file = codecs.open("yasser.txt", "w", "utf-8")
