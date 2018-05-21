b5cec0e175fa113678bf597e80d9ba3835444409
yasserkabbout <yasserkabbout@gmail.com>
2018-05-21 15:43:21 +0300

    Api integration with api clients

---
+++ front-end/assets/api/insertGreen.php
@@ -0,0 +1,31 @@
+<html>
+
+<head> </head>
+
+<?php
+$servername = "yasserkabboutcom.ipagemysql.com";
+$username = "yasser";
+$password = "yasser";
+$dbname = "twee_lights";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO winners_colors (store_id, winner_color)
+VALUES (1, 'GREEN')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+</body>
+
+</html>
